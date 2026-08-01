9565c0f975d3826292359cf27a06f4f8b99c5e5e
Fairy23-source <solankifairy612@gmail.com>
2026-08-01 12:25:13 +0530

    Add my file

---
+++ searchinsertionposition.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int searchInsert(vector<int>& nums, int target) {
+    int left = 0;
+    int right = nums.size() - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] == target) {
+            return mid;
+        }
+        else if (nums[mid] < target) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+
+    return left;
+}
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    vector<int> nums(n);
+
+    cout << "Enter sorted elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    int target;
+    cout << "Enter target: ";
+    cin >> target;
+
+    int result = searchInsert(nums, target);
+
+    cout << "Search Insert Position: " << result << endl;
+
+    return 0;
+}
