6a64050283d2748005c746903dae43e56a2212e6
Fairy23-source <solankifairy612@gmail.com>
2026-08-02 09:04:55 +0530

    Add Remove Duplicates solution

---
+++ removeduplicate.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int removeDuplicates(vector<int>& nums) {
+    if (nums.empty()) {
+        return 0;
+    }
+
+    int i = 0;
+
+    for (int j = 1; j < nums.size(); j++) {
+
+        if (nums[j] != nums[i]) {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+
+    return i + 1;
+}
+
+int main() {
+    int n;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    vector<int> nums(n);
+
+    cout << "Enter sorted elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    int k = removeDuplicates(nums);
+
+    cout << "Number of unique elements: " << k << endl;
+
+    cout << "Array after removing duplicates: ";
+    for (int i = 0; i < k; i++) {
+        cout << nums[i] << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
