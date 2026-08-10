26f4f932a1022415dd8de8fb8f1403676a4e6983
Fairy Solanki <solankifairy612@gmail.com>
2026-08-10 16:13:47 +0530

    Time: 0 ms (100%), Space: 14.2 MB (59.83%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+
+    void solve(vector<int>& candidates, int target, int start,
+               vector<int>& current, vector<vector<int>>& answer) {
+
+        if (target == 0) {
+            answer.push_back(current);
+            return;
+        }
+
+        if (target < 0) {
+            return;
+        }
+
+        for (int i = start; i < candidates.size(); i++) {
+
+            current.push_back(candidates[i]);
+
+            solve(candidates, target - candidates[i], i,
+                  current, answer);
+
+            current.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+
+        vector<vector<int>> answer;
+        vector<int> current;
+
+        solve(candidates, target, 0, current, answer);
+
+        return answer;
+    }
+};
