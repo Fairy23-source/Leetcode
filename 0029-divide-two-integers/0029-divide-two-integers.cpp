6e7cc694a843e6557e212c7d3cf401b69e8bebac
Fairy Solanki <solankifairy612@gmail.com>
2026-09-04 12:13:56 +0530

    Update 0029-divide-two-integers.cpp

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -5,7 +5,7 @@ public:
         // Special case: overflow
         if (dividend == INT_MIN && divisor == -1) {
             return INT_MAX;
-        }
+              }
 
         long long a = abs((long long)dividend);
         long long b = abs((long long)divisor);
